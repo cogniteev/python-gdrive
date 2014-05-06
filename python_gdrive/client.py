6890d6a218b4ac516447628e7acf9d0db3fed2a9
tsanch3z <mail.tsanchez@gmail.com>
2014-05-06 18:01:56 +0200

    first working version

---
+++ python_gdrive/client.py
@@ -0,0 +1,156 @@
+import requests
+
+
+class GoogleDrive(object):
+    """ A simple API wrapper for google drive
+
+    """
+
+    def __init__(self, token, refresh_token, client_id, client_secret,
+                 refresh_callback=None):
+        """
+
+        :param token: the granted access token
+        :param refresh_token: the granted refresh token
+        :param client_id: Oauth client id
+        :param client_secret: Oauth client secret
+        :param refresh_callback: callable to handle credentials refreshing
+        """
+        self.token = token
+        self.refresh_token = refresh_token
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.refresh_callback = refresh_callback
+
+    def __refresh_token(self):
+        """ Handles the refresh process and update with newly acquired values
+
+        """
+        refresh_data = {
+            'refresh_token': self.refresh_token,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+            'grant_type': 'refresh_token'
+        }
+
+        response = requests.post('https://accounts.google.com/o/oauth2/token',
+                                 data=refresh_data).json()
+
+        self.token = response['access_token']
+
+        if self.refresh_callback:
+            self.refresh_callback(self.token, self.refresh_token)
+
+    def __token_header(self):
+        """ Create an HTTP header containing the access token
+
+
+        :return: the properly formatted header
+        """
+        return {'Authorization': 'Bearer {token}'.format(token=self.token)}
+
+    def __request(self, method, path, params=None, refreshed=False):
+        """ submit a request to the google drive API server
+
+        :param method: HTTP verb to use
+        :param path: REST resource's path
+        :param params: parameters for the request
+        :param refreshed: True if the call is made after a token refresh
+        :return: Server's raw response
+        """
+        url = 'https://www.googleapis.com/drive/v2/{path}'.format(path=path)
+
+        headers = self.__token_header()
+
+        response = requests.request(method, url, params=params, headers=headers)
+
+        if response.status_code == 401:
+            if refreshed:
+                return response
+            self.__refresh_token()
+            return self.__request(method, path, refreshed=True)
+        else:
+            return response
+
+    def download_content(self, url, refreshed=False):
+        """ download a file content
+
+        :param url: the url to download the file from
+        :param refreshed: True if the call is made after a token refresh
+        :return: file's content
+        """
+        headers = self.__token_header()
+
+        response = requests.request('get', url, headers=headers)
+
+        if response.status_code == 401:
+            if refreshed:
+                return response.content
+            self.__refresh_token()
+            return self.download_content(url, refreshed=True)
+        else:
+            return response.content
+
+    def get_user_files(self, count=100, page_token=None):
+        """ Retrieve user's files metadata
+
+        :param count: number of file to get
+        :param page_token: optional, page token pagination parameter
+        :return: Server's raw response
+        """
+        params = {
+            'q': "mimeType != 'application/vnd.google-apps.folder'",
+            'maxResults': count,
+            'pageToken': page_token
+        }
+        return self.__request('get', 'files', params=params)
+
+    def get_user_files_generator(self, count=100):
+        """ Create a generator to iterate through user's files
+
+        :param count: number of files metadata to get at each iteration
+        """
+        next_page_token = None
+        while True:
+            response = self.get_user_files(count=count,
+                                           page_token=next_page_token).json()
+            next_page_token = response.get('nextPageToken', None)
+            for item in response['items']:
+                yield item
+
+            if not next_page_token:
+                break
+
+    def get_file_comments(self, file_id, count=100, page_token=None):
+        """ Retrieve a list of comments for a given file
+
+        :param file_id: the file to get associated comments
+        :param count: number of comments to get
+        :param page_token: page token pagination parameter
+        :return: Server's raw response
+        """
+        params = {
+            'maxResults': count,
+            'pageToken': page_token
+        }
+        return self.__request('get', 'files/{id}/comments'.format(id=file_id),
+                              params=params)
+
+    def get_file_comments_generator(self, file_id, count=100):
+        """ Create a generator to iterate through comments
+
+        :param file_id: the file to get associated comments
+        :param count: number of comments to get at each iteration
+        """
+        next_page_token = None
+        while True:
+            response = self.get_file_comments(file_id, count=count,
+                                              page_token=next_page_token).json()
+            next_page_token = response.get('nextPageToken', None)
+            for item in response['items']:
+                yield item
+
+            if not next_page_token:
+                break
+
+
